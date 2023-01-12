ad56222edbe7fa9c44165e3afee8f83c2e8aa431
TACAMP <112141419+TACAMP@users.noreply.github.com>
2023-01-12 19:03:55 +0900

    fix top view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :admins
+  devise_for :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root 
+
+  get 'top' => 'homes#top'
+
+  root 'homes#top'
 end

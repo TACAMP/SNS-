f8fa9c4daedcf94586ecdf83a467c17afdd0b465
TACAMP <112141419+TACAMP@users.noreply.github.com>
2023-01-13 13:42:41 +0900

    fix migration

--- db/migrate/20230113042318_create_camp_tools.rb
+++ db/migrate/20230113042318_create_camp_tools.rb
@@ -2,7 +2,7 @@ class CreateCampTools < ActiveRecord::Migration[7.0]
   def change
     create_table :camp_tools do |t|
       t.references :post, null: false, foreign_key: true
-      t.string :name
+      t.string :name,null: false
 
       t.timestamps
     end
